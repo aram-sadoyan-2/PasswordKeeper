458d51cd8b6783fc312678ca1fac46973e45b9a8
aram-sadoyan-2 <aram.k.sadoyan@gmail.com>
2026-03-14 22:49:08 +0400

    initial commit

--- app/src/main/java/com/amroad/passwkeeper/MainActivity.kt
+++ app/src/main/java/com/amroad/passwkeeper/MainActivity.kt
@@ -4,11 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.amroad.passwkeeper.ui.AppRoot
 import com.amroad.passwkeeper.ui.theme.PasswordKeeperTheme
 
@@ -19,24 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PasswordKeeperTheme {
                 AppRoot()
-
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PasswordKeeperTheme {
-        Greeting("Android")
     }
 }
